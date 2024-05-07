db88332d92d5fc46eab8538879e6fda8bfbe6f42
Sibionca <sibien28@gmail.com>
2024-05-07 13:38:50 -0300

    Criação rota login, inclusão forgotPassword

---
+++ src/dataBase/usuarioLogin.ts
@@ -0,0 +1,79 @@
+import knex from "knex";
+import { UserModel } from "../models/model";
+import { comparePasswords, generateToken } from "../utils/bcrypFunctions"
+import * as nodemailer from 'nodemailer';
+
+require("dotenv").config();
+
+export class UserLogin{
+
+  public static loginUser(email: string, senha: string): Promise<UserModel> {
+    return new Promise((resolve, reject) => {
+      knex("usuarios")
+        .select("*")
+        .where("email", email)
+        .then((usuarioBanco: UserModel | any) => {
+
+          if (!usuarioBanco) reject(new Error("Nenhum usuário encontrado com este email!"));
+          
+          if (comparePasswords(senha, usuarioBanco.password)) resolve(usuarioBanco);
+
+          reject(new Error("Senha incorreta!"));         
+        })
+        .catch((erro: any) => {
+          reject(erro);
+        });
+    });
+  }
+
+  public static async forgotPassword(email: string): Promise<boolean> {
+    try {
+      const user: UserModel | undefined = await knex("usuarios")
+        .where("email", email)
+        .first();
+
+      if (!user) throw new Error("Usuário não encontrado");
+
+      const resetToken: string = generateToken();
+      const now: Date = new Date();
+      now.setHours(now.getHours() + 1);
+
+      await knex("usuarios").where("id", user.id).update({
+        passwordResetToken: resetToken,
+        passwordResetExpires: now,
+      });
+
+      setTimeout(async () => {
+        await knex("usuarios").where("id", user.id).update({
+          passwordResetToken: null,
+          passwordResetExpires: null,
+        });
+      }, 3600000);
+
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        auth: {
+          user: process.env.USER_GMAIL,
+          pass: process.env.USER_GMAIL_PASSWORD,
+        },
+      });
+
+      const mailOptions = {
+        from: "contaparaoaplicativo46@gmail.com",
+        to: `${user.email}`,
+        subject: "Recuperação de Senha",
+        html: `<p>Olá ${user.email},</p>
+          <p>Você solicitou a redefinição da sua senha. Utilize este token para recuperar a senha:</p>
+          <p>${resetToken}</p>
+          <p>Se você não solicitou a redefinição de senha, ignore este e-mail.</p>`,
+      };
+
+      await transporter.sendMail(mailOptions);
+      return true;
+    } catch (error) {
+      throw new Error("Erro ao enviar o e-mail de recuperação de senha");
+    }
+  }
+
+}
--- src/models/model.ts
+++ src/models/model.ts
@@ -16,6 +16,7 @@ export interface UserModel {
   id: string;
   name: string;
   email: string;
+  cpf: string;
   password: string;
   passwordResetToken: string;
   passwordResetExpires: Date;
