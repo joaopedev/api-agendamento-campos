fcdbbed5fcd4e976985142c71aaef743e6fcb37a
Sibionca <sibien28@gmail.com>
2024-07-24 11:33:54 -0300

    correção espaçamento

--- src/routers/private/blocking.ts
+++ src/routers/private/blocking.ts
@@ -10,112 +10,112 @@ import { Usuario } from '../../dataBase/usuario';
 
 export = (app: Application) => {
 
-    app.get(
-        '/private/block_scheduling',
-        async (req: Request, res: Response, next: NextFunction) => {
+  app.get(
+    '/private/block_scheduling',
+    async (req: Request, res: Response, next: NextFunction) => {
 
-            await BloqueioAgendamento.getBloqueiosAgendamento()
-            .then(bloqueios => {
-                res.json({
-                  message: 'bloqueios recuperados com sucesso',
-                  contas: bloqueios,
-                });
-              })
-              .catch(erro => {
-                next(createError(HTTP_ERRORS.VALIDACAO_DE_DADOS, erro));
-              });
-        }
-    );
+      await BloqueioAgendamento.getBloqueiosAgendamento()
+      .then(bloqueios => {
+        res.json({
+          message: 'bloqueios recuperados com sucesso',
+          contas: bloqueios,
+        });
+      })
+      .catch(erro => {
+        next(createError(HTTP_ERRORS.VALIDACAO_DE_DADOS, erro));
+      });
+    }
+  );
 
-    app.get(
-        '/private/block_schedulingById/:id',
-        async (req: Request, res: Response, next: NextFunction) => {
+  app.get(
+    '/private/block_schedulingById/:id',
+    async (req: Request, res: Response, next: NextFunction) => {
 
-            await BloqueioAgendamento.getBloqueioAgendamentoById(req.body.id)
-            .then(bloqueio => {
-                res.json({
-                  message: 'bloqueio recuperados com sucesso',
-                  contas: bloqueio,
-                });
-              })
-              .catch(erro => {
-                next(createError(HTTP_ERRORS.VALIDACAO_DE_DADOS, erro));
-              });
-        }
-    );
+      await BloqueioAgendamento.getBloqueioAgendamentoById(req.body.id)
+      .then(bloqueio => {
+        res.json({
+          message: 'bloqueio recuperados com sucesso',
+          contas: bloqueio,
+        });
+      })
+      .catch(erro => {
+        next(createError(HTTP_ERRORS.VALIDACAO_DE_DADOS, erro));
+      });
+    }
+  );
 
-    app.post(
-        "/private/registerBlock",
-        body("usuario_id").notEmpty(),
-        body("data").notEmpty(),
-        body("tipo_bloqueio").notEmpty(),
-        body("cras").notEmpty(),
+  app.post(
+    "/private/registerBlock",
+    body("usuario_id").notEmpty(),
+    body("data").notEmpty(),
+    body("tipo_bloqueio").notEmpty(),
+    body("cras").notEmpty(),
 
-        async (req: Request, res: Response, next: NextFunction) => {
-            const errors = validationResult(req);
-      
-            if (!errors.isEmpty()) {
-              const msgBugada = JSON.stringify(errors.array()[0]);
-              const msgFormatada = msgBugada.substring(1, msgBugada.length - 1).replace(/\\/g, '');
-      
-              return next(createError(HTTP_ERRORS.VALIDACAO_DE_DADOS,
-                msgFormatada)
-              );
-            }
+    async (req: Request, res: Response, next: NextFunction) => {
+      const errors = validationResult(req);
 
-            const bloqueio: BloqueioAgendamentoModel = { ...req.body }
-            bloqueio.data = new Date(req.body.data);
+      if (!errors.isEmpty()) {
+        const msgBugada = JSON.stringify(errors.array()[0]);
+        const msgFormatada = msgBugada.substring(1, msgBugada.length - 1).replace(/\\/g, '');
 
-            if (!bloqueio.usuario_id || !bloqueio.data) {
-                const erro = bloqueio.usuario_id ? "data" : "usuario_id"; 
-                return next(
-                  createError(HTTP_ERRORS.BAD_REQUEST, `${erro} inválido`)
-                );
-            }
+        return next(createError(HTTP_ERRORS.VALIDACAO_DE_DADOS,
+          msgFormatada)
+        );
+      }
 
-            await BloqueioAgendamento.createBloqueioAgendamento(bloqueio)
-            .then((result) => {
-                res.json({ message: "bloqueio criado com sucesso!", result });
-            })
-            .catch((erro) => {
-                console.error(erro);
-                next(createError(HTTP_ERRORS.ERRO_INTERNO, tratarErro(erro)));
-            }); 
-        }
-    )
+      const bloqueio: BloqueioAgendamentoModel = { ...req.body }
+      bloqueio.data = new Date(req.body.data);
 
-    app.put(
-        '/private/updateBlock/',
-        async (req: Request, res: Response, next: NextFunction) => {
-            let bloqueioId = req.query.id?.toString() ?? "";
-            let usuarioParaAlteracao = req.query.usuario_id?.toString() ?? "";
+      if (!bloqueio.usuario_id || !bloqueio.data) {
+        const erro = bloqueio.usuario_id ? "data" : "usuario_id"; 
+        return next(
+          createError(HTTP_ERRORS.BAD_REQUEST, `${erro} inválido`)
+        );
+      }
 
-            let bloqueio: BloqueioAgendamentoModel = await BloqueioAgendamento.getBloqueioAgendamentoById(bloqueioId);
-            let usuario: UserModel = await Usuario.getUserById(usuarioParaAlteracao);
+      await BloqueioAgendamento.createBloqueioAgendamento(bloqueio)
+      .then((result) => {
+        res.json({ message: "bloqueio criado com sucesso!", result });
+      })
+      .catch((erro) => {
+        console.error(erro);
+        next(createError(HTTP_ERRORS.ERRO_INTERNO, tratarErro(erro)));
+      }); 
+    }
+  )
 
-            if(!usuario || !bloqueio.id){
-                const erro = usuario ? "o agendamentoId é" : "o usuario_id é"; 
-                return next(
-                  createError(HTTP_ERRORS.BAD_REQUEST, `${erro} inválido`)
-                );
-            }
+  app.put(
+    '/private/updateBlock/',
+    async (req: Request, res: Response, next: NextFunction) => {
+      let bloqueioId = req.query.id?.toString() ?? "";
+      let usuarioParaAlteracao = req.query.usuario_id?.toString() ?? "";
 
-            if(usuario.tipo_usuario != TipoUsuario.superAdmin) 
-                return next(createError(HTTP_ERRORS.BAD_REQUEST, "Você não tem permissão para atualizar esse bloqueio!"))
+      let bloqueio: BloqueioAgendamentoModel = await BloqueioAgendamento.getBloqueioAgendamentoById(bloqueioId);
+      let usuario: UserModel = await Usuario.getUserById(usuarioParaAlteracao);
 
-            if(!bloqueio.ativo)
-                return next(createError(HTTP_ERRORS.BAD_REQUEST, "Este bloqueio ja foi cancelado!"))
+      if(!usuario || !bloqueio.id){
+        const erro = usuario ? "o agendamentoId é" : "o usuario_id é"; 
+        return next(
+          createError(HTTP_ERRORS.BAD_REQUEST, `${erro} inválido`)
+        );
+      }
 
-            bloqueio = {...bloqueio, ...req.body}
+      if(usuario.tipo_usuario != TipoUsuario.superAdmin) 
+          return next(createError(HTTP_ERRORS.BAD_REQUEST, "Você não tem permissão para atualizar esse bloqueio!"))
 
-            await BloqueioAgendamento.updateBloqueioAgendamento(bloqueio)
-            .then(() => {
-                res.json({ message: "Bloqueio atualizado com sucesso!" });
-              })
-              .catch((erro) => {
-                  console.error(erro);
-                  next(createError(HTTP_ERRORS.ERRO_INTERNO, tratarErro(erro)));
-              }); 
-        }
-    )
+      if(!bloqueio.ativo)
+          return next(createError(HTTP_ERRORS.BAD_REQUEST, "Este bloqueio ja foi cancelado!"))
+
+      bloqueio = {...bloqueio, ...req.body}
+
+      await BloqueioAgendamento.updateBloqueioAgendamento(bloqueio)
+      .then(() => {
+        res.json({ message: "Bloqueio atualizado com sucesso!" });
+      })
+      .catch((erro) => {
+        console.error(erro);
+        next(createError(HTTP_ERRORS.ERRO_INTERNO, tratarErro(erro)));
+      }); 
+    }
+  )
 }
